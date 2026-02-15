1101cefde6424f0a50f0dfa581a8df5c0a04c2b8
abdoelkasby58 <abdoelkasby58@example.com>
2026-02-15 12:52:47 +0200

    KAsbbbbbbbbbbbbb

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from "@/view/HomePage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: HomePage,
+    meta: {
+      title: "Home Page",
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+export default router;
